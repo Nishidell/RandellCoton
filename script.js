84956d4321bd87841d7e5c0b5727c90f558a3a01
Franc Randell Coton <166776404+Nishidell@users.noreply.github.com>
2026-03-11 11:36:43 +0800

    add gradient deisgn and polish the mobile view

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function typeWriter() {
         // If there are still letters left to type...
         if (charIndex < textToType.length) {
-            // Add the next letter to the screen
             typingElement.innerHTML += textToType.charAt(charIndex);
             charIndex++;
             // Wait 50 milliseconds, then type the next letter!
@@ -16,20 +15,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Start the typing animation!
+    // Start
     typeWriter();
 
     // --- Navbar Scroll Effect ---
     const navbar = document.getElementById('navbar');
 
-    // Listen for the user scrolling the page
+    // logic for naviagation bar background change on scroll
     window.addEventListener('scroll', () => {
-        // If they scroll down more than 50 pixels...
         if (window.scrollY > 50) {
-            // Add the solid cream background
+           
             navbar.classList.add('scrolled');
         } else {
-            // If they are at the very top, remove it to make it transparent again
+          
             navbar.classList.remove('scrolled');
         }
     });
